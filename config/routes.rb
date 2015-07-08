d772fc6de94992fd81edfe8f2e7be386fcd1d21a
Jay Manalansan <jaypatrickm@gmail.com>
2015-07-08 14:18:01 -0700

    committing html/css portion

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+
+  get 'comments/new'
+
+  get 'comments/create'
+
+  get 'messages/index'
+  get 'messages/' => 'messages#index'
+  get 'messages/new'
+
+  get 'messages/create'
+
+  get 'users/index'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
